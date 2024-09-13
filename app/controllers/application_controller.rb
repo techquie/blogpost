8342a66a85c27600ece3b3b358c5394fd5831c62
techquie <kundanverma265@gmail.com>
2024-09-13 22:43:28 +0530

    Added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, :if => :skip_authentication_stories
+
+  private
+
+  def skip_authentication_stories
+    false if action_name == 'index' && controller_name == 'stories'
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role, :active])
+  end
 end
