a80bf72d0be60a881d2da6078e154b65dc458069
techquie <kundanverma265@gmail.com>
2024-09-14 18:57:03 +0530

    removed unsued codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role, :active])
   end
 
-  def after_sign_out_path_for(resource_or_scope)
-    # You can change this to any path you want, such as:
-    # new_user_session_path, or some other custom page
-    root_path  # By default, this redirects to the root of the application
-  end
-
 end
