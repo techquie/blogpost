e5ffad26d78f8186a114367a56de880abd6f8ea8
techquie <kundanverma265@gmail.com>
2024-09-15 02:01:11 +0530

    Added cancancan authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, :if => :skip_authentication_stories
+  load_and_authorize_resource
 
   private
 
